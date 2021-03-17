ddf7cc2b4d3336cd675b6e33edb75800c691d230
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-17 23:43:38 +0100

    Checked if argument passed matches property if yes return value

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,12 +1,9 @@
 function myFunction(myObj, checkProp) {
-    myObj = {
-        title: "Titanic",
-        song: "My Heart Will Go On",
-        genre: "drama"
-    };
 
     if(myObj.hasOwnProperty(checkProp)) {
         return myObj[checkProp];
+    } else {
+        return "Not found";
     }
 }
 
